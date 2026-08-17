3416952046ca1ce4a2920192501b90bc8049d682
Jayaprakash <jayaprakash161107@gmail.com>
2026-08-17 09:44:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,119 @@
+
+
+//     1 — Looping Through an Array
+
+
+
+//   USING FOR LOOP
+
+
+// const numbers = [10, 20, 30, 40];
+
+// for (i = 0 ; i < numbers.length ; i++){
+    
+//     console.log(numbers[i]);
+    
+    
+// }
+
+
+
+// FOR EACH
+
+
+
+// const marks = [10, 20, 30, 40];
+
+//  marks.forEach ((num) => {
+//     console.log(num);
+    
+// });
+
+
+///  for in loop
+
+
+// let number = [10,20,30,40]
+     
+//   for(let i in number ){
+//     console.log(number[i]);
+    
+//   }
+
+
+//  for of loop
+
+
+// let students = [10,20,30,40]
+
+
+// for(let student of students ){
+//      console.log(student);
+     
+// }
+
+
+
+//    2 — Loop Through an Object
+
+
+  
+// const student = {
+//   name: "Bala",
+//   age: 21,
+//   grade: "A"
+// };
+
+// for(const i in student){
+//     console.log(i + ":" + student[i]);
+    
+// }
+
+
+
+//   3 — Using map()
+
+
+// const marks = [50, 60, 70, 80];
+
+
+// const mark = marks.map (mark => mark - 10 );
+  
+//  console.log(mark);
+
+
+
+//      4 — Using filter()
+
+
+
+// const value = [5, 12, 8, 25, 3, 15];
+
+
+// const filterdvalue = value.filter((value) =>{
+
+//      return value <=  10 ;
+   
+    
+// })
+//  console.log(filterdvalue);
+
+
+
+
+//     5 — Using reduce()
+
+
+// const nums = [5, 10, 15, 20];
+
+// const reducenum = nums.reduce((sum,nums) => sum + nums , 0 );
+
+
+// console.log( reducenum);
+
+
+
+
+
+
+
